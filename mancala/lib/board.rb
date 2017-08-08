0967cf50f950df3b55b0386f1bf9fd1d3ba0a519
Trevor Uptain <trevoru@uw.edu>
2017-08-08 09:41:26 -0700

    Completed movement methods for board

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -2,20 +2,51 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { Array.new }
+    @player1, @player2 = name1, name2
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |ele, i|
+      4.times { ele << :stone } unless i == 6 || i == 13
+    end
   end
 
   def valid_move?(start_pos)
+    if start_pos < 0 || start_pos > 12 || @cups[start_pos].empty?
+      raise 'Invalid starting cup'
+    end
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    idx = start_pos
+
+    until stones.empty?
+      idx += 1
+      idx = 0 if idx > 13
+      if @player1 == current_player_name
+        @cups[idx] << stones.shift unless idx == 13
+      elsif @player2 == current_player_name
+        @cups[idx] << stones.shift unless idx == 6
+      end
+    end
+    render
+    next_turn(idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -27,6 +58,13 @@ class Board
   end
 
   def one_side_empty?
+    for i in 0..5
+      return false if @cups[i].count > 0
+    end
+    for i in 7..12
+      return false if @cups[i].count > 0
+    end
+    true
   end
 
   def winner
