3be6a9d7cf952625a7c2f6d8985990e0595de27a
Trevor Uptain <trevoru@uw.edu>
2017-08-24 18:18:01 -0700

    Removed folder

--- 99-cats-2/db/migrate/20170824233059_fix_cats.rb
+++
@@ -1,6 +0,0 @@
-class FixCats < ActiveRecord::Migration[5.1]
-  def change
-    add_column :cats, :user_id, :integer
-    add_index :cats, :user_id
-  end
-end
--- 99-cats-2/db/migrate/20170825002629_fix_rental_request.rb
+++
@@ -1,6 +0,0 @@
-class FixRentalRequest < ActiveRecord::Migration[5.1]
-  def change
-    add_column :cat_rental_requests, :user_id, :integer
-    add_index :cat_rental_requests, :user_id
-  end
-end
