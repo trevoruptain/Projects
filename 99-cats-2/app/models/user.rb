8b203b26787979d257c0cbafa6160b5569b4457e
Trevor Uptain <trevoru@uw.edu>
2017-08-24 18:07:12 -0700

    Added W4D4 Homework

---
+++ 99-cats-2/app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :session_token, :username, :password_digest, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  has_many :cats
+  has_many :cat_rental_requests
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token!
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token!
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    @password = password
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token!
+  end
+end
